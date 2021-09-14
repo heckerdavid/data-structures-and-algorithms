236d3d4c3e61801706f5f0f6c6d9dd70f9138de4
David Hecker <HECKERDW@GMAIL.COM>
2021-09-14 16:25:47 -0700

    Completed Q3.

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -36,7 +36,9 @@ Write a function named addQuestion that, given an array of strings, uses map to 
 ------------------------------------------------------------------------------------------------ */
 
 const addQuestion = (arr) => {
-  // Solution code here...
+  let makeQuestionArray = arr.map( str => str + '?');
+
+  return makeQuestionArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -217,7 +219,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should add a question mark to the end of each string', () => {
     expect(addQuestion(['hello', '301', 'students'])).toStrictEqual(['hello?', '301?', 'students?']);
   });
