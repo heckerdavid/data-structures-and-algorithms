c7927d4d225c5fb211bb9baefb65477685459c2e
David Hecker <HECKERDW@GMAIL.COM>
2021-09-13 17:43:29 -0700

    Completed Q3.

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -41,7 +41,11 @@ Use `forEach` to loop over the input array. The modified strings should each be 
 ------------------------------------------------------------------------------------------------ */
 
 const allUpperCase = (arr) => {
-  // Solution code here...
+  const tallArray = [];
+
+  arr.forEach((string) => tallArray.push(string.toUpperCase()));
+
+  return tallArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -149,7 +153,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should return an array of uppercase strings', () => {
     expect(allUpperCase(['hi', 'how', 'are', 'you'])).toStrictEqual(['HI', 'HOW', 'ARE', 'YOU']);
   });
