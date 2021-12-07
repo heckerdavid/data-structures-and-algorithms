6abcb4dbc890d0c33d060251b28b4244511580f5
David Hecker <heckerdw@gmail.com>
2021-12-06 16:51:19 -0800

    Completed Challenge 06.

--- python/linked_list/README.md
+++ python/linked_list/README.md
@@ -1,9 +1,11 @@
 # Singly Linked List
 <!-- Short summary or background information -->
 Code Challenge 05
-
+Code Challenge 06
 ## Challenge
 <!-- Description of the challenge -->
+* Code Challenge 05
+
 Node
     Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
 
@@ -28,6 +30,19 @@ to string
     Arguments: none
     Returns: a string representing all the values in the Linked List, formatted as:
         "{ a } -> { b } -> { c } -> NULL"
+
+* Code Challenge 05
+Write the following methods for the Linked List class:
+
+append
+    arguments: new value
+    adds a new node with the given value to the end of the list
+insert before
+    arguments: value, new value
+    adds a new node with the given new value immediately before the first node that has the value specified
+insert after
+    arguments: value, new value
+    adds a new node with the given new value immediately after the first node that has the value specified
 
 ## Approach & Efficiency
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -35,6 +35,47 @@ class LinkedList:
         """Inserts given value to linked list head O(1)"""
         self.head = Node(value, self.head)
 
+
+    def insert_before(self, pivot, value) -> None:
+        current = self.head
+        previous = self.head
+
+        if self.head.value == pivot:
+            self.head = Node(value, self.head)
+            return None
+
+        while current:
+            if current.value == pivot:
+                previous.next = Node(value, current)
+                return None
+            previous = current
+            current = current.next
+        raise Exception(f'{pivot} not in list.')
+
+
+    def insert_after(self, pivot, value):
+        current = self.head
+        previous = self.head
+
+        while current:
+            if current.value == pivot:
+                current.next = Node(value, current.next)
+                return None
+            previous = current
+            current = current.next
+
+
+    def append(self, value) -> None:
+        current = self.head
+        if current is None:
+                self.head = Node(value)
+        while current:
+            if current.next is None:
+                current.next = Node(value)
+                break
+            current = current.next
+
+
     def includes(self, value: any) -> bool:
         """Indicates whether that value exists as a Node’s value somewhere within the list."""
         current = self.head
