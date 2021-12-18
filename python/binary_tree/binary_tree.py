f092d12d59b58c89506a3f07de53aa5485ff769c
David Hecker <heckerdw@gmail.com>
2021-12-18 14:01:21 -0800

    Added pre, in, and post order methods.

--- python/binary_tree/binary_tree.py
+++ python/binary_tree/binary_tree.py
@@ -9,4 +9,50 @@ class BinaryTree:
         if self.root.left is None:
             self.root.left = Node(value)
             return
-        self.root.right = Node(value)
+        elif self.root.right is None:
+            self.root.right = Node(value)
+
+    def pre_order(self) -> list:
+        output = []
+
+        def walk(root=self.root):
+            if root is None:
+                return
+            output.append(root.value)
+            walk(root.left)
+            walk(root.right)
+            return
+
+        walk()
+
+        return output
+
+    def in_order(self) -> list:
+        output = []
+
+        def walk(root=self.root):
+            if root is None:
+                return
+            walk(root.left)
+            output.append(root.value)
+            walk(root.right)
+            return
+
+        walk()
+
+        return output
+
+    def post_order(self) -> list:
+        output = []
+
+        def walk(root=self.root):
+            if root is None:
+                return
+            walk(root.left)
+            walk(root.right)
+            output.append(root.value)
+            return
+
+        walk()
+
+        return output
--- python/tests/test_binary_tree.py
+++ python/tests/test_binary_tree.py
@@ -4,8 +4,8 @@ from binary_tree.node import Node
 '''
 Can successfully instantiate an empty tree
 Can successfully instantiate a tree with a single root node
-
 Can successfully add a left child and right child to a single root node
+
 Can successfully return a collection from a preorder traversal
 Can successfully return a collection from an inorder traversal
 Can successfully return a collection from a postorder traversal'''
@@ -66,4 +66,34 @@ def test_add_right_to_tree():
 
     actual = tree.root.right.value
     expected = 5
+    assert actual == expected
+
+def test_preorder():
+    tree = BinaryTree()
+    tree.root = Node(3)
+    tree.root.left = Node(2)
+    tree.root.right = Node(4)
+
+    actual = tree.pre_order()
+    expected = [3, 2, 4]
+    assert actual == expected
+
+def test_in_order():
+    tree = BinaryTree()
+    tree.root = Node(3)
+    tree.root.left = Node(2)
+    tree.root.right = Node(4)
+
+    actual = tree.in_order()
+    expected = [2, 3, 4]
+    assert actual == expected
+
+def test_post_order():
+    tree = BinaryTree()
+    tree.root = Node(3)
+    tree.root.left = Node(2)
+    tree.root.right = Node(4)
+
+    actual = tree.post_order()
+    expected = [2, 4, 3]
     assert actual == expected
