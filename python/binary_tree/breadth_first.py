2296eff0c2db32a78324ef03da64b6a831bb3827
David Hecker <heckerdw@gmail.com>
2021-12-21 14:48:43 -0800

    Added breadth first and associated tests to binary tree.

--- python/binary_tree/breadth_first.py
+++ python/binary_tree/breadth_first.py
@@ -3,3 +3,16 @@ from stack_and_queue.queue import Queue
 def breadth_first(tree) -> list:
     if tree.root is None:
         return []
+
+    value_list = []
+    queue = Queue()
+
+    queue.enqueue(tree.root)
+
+    while queue.peek():
+        current = queue.dequeue()
+        queue.enqueue(current.left)
+        queue.enqueue(current.right)
+        value_list.append(current.value)
+
+    return value_list
--- python/tests/test_breadth_first_tree.py
+++ python/tests/test_breadth_first_tree.py
@@ -12,3 +12,51 @@ def test_empty_tree():
     expected = []
     assert actual == expected
 
+def test_root():
+    tree = BinaryTree(Node(5))
+
+    actual = breadth_first(tree)
+    expected = [5]
+    assert actual == expected
+
+def test_left():
+    tree = BinaryTree(Node(5))
+    tree.root.left = Node(4)
+
+    actual = breadth_first(tree)
+    expected = [5, 4]
+    assert actual == expected
+
+
+def test_right():
+    tree = BinaryTree(Node(5))
+    tree.root.left = Node(4)
+    tree.root.right = Node(6)
+
+    actual = breadth_first(tree)
+    expected = [5, 4, 6]
+    assert actual == expected
+
+
+def test_level_3():
+    tree = BinaryTree(Node(5))
+    tree.root.left = Node(4)
+    tree.root.right = Node(6)
+    tree.root.left.left = Node(2)
+
+    actual = breadth_first(tree)
+    expected = [5, 4, 6, 2]
+    assert actual == expected
+
+def test_level_4():
+    tree = BinaryTree(Node(1))
+    tree.root.left = Node(2)
+    tree.root.right = Node(3)
+    tree.root.left.left = Node(4)
+    tree.root.left.right = Node(5)
+    tree.root.right.left = Node(6)
+    tree.root.right.right = Node(7)
+
+    actual = breadth_first(tree)
+    expected = [1, 2, 3, 4, 5, 6, 7]
+    assert actual == expected
