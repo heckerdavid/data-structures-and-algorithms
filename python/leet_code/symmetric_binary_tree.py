0ad212823c744a9c1e2f0876e89dc99bc496c8a5
David Hecker <heckerdw@gmail.com>
2022-03-13 13:25:10 -0700

    Added symmetric tree attempt two.

--- python/leet_code/symmetric_binary_tree.py
+++ python/leet_code/symmetric_binary_tree.py
@@ -1,7 +1,7 @@
 # given a binary tree ROOT, check if it is symmetric around its center (a mirror of itself)
 
 
-def symmetric_tree(root):
+def symmetric_tree_first_attempt(root):
     left = in_order_list(root.left)
     right = in_order_list(root.right)
 
@@ -24,3 +24,14 @@ def in_order_list(root):
 
     return output
 
+# second attempt
+def symmetric_tree(root):
+    return is_symmetric(root, root)
+
+def is_symmetric(root1, root2):
+    if root1 is None and root2 is None:
+        return True
+    elif root1 is None or root2 is None:
+        return False
+
+    return root1.value == root2.value and is_symmetric(root1.left, root2.right) and is_symmetric(root1.right, root2.left)
